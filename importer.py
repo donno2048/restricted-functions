a21aaef7f42c15ba81840b7e33251959acc7ce6c
iam-py-test <84232764+iam-py-test@users.noreply.github.com>
2021-06-16 09:35:51 -0400

    Update importer.py

--- importer.py
+++ importer.py
@@ -1,10 +1,19 @@
-import importlib
-def importer(name, *args):
-    M = importlib.__import__(name, *args)
-    if name == 'os':
-        del M.system
-    elif name == 'subprocess':
-        del M.run
-    return M
-__builtins__.__dict__['__import__'] = importer
-del importlib
+try:
+    import importlib
+    def importer(name, *args):
+        M = importlib.__import__(name, *args)
+        if name == 'os':
+            del M.system
+        elif name == 'subprocess':
+            del M.run
+        return M
+    __builtins__.__dict__['__import__'] = importer
+    del importlib
+ except Exception as err:
+    with open("output.txt",'w') as f:
+        f.write("Error: {}".format(err))
+        f.close()
+else:
+    with open("output.txt","w") as f:
+        f.write("No errors")
+        f.close()
