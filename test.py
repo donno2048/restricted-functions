7d50dc2f3d06bf1f0cb4c523bb20f9bd33e6fccf
Elisha Hollander <just4now666666@gmail.com>
2021-06-16 16:43:14 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,13 @@
-import importer
+import importlib
+def importer(name, *args):
+    M = importlib.__import__(name, *args)
+    if name == 'os':
+        del M.system
+    elif name == 'subprocess':
+        del M.run
+    return M
+__builtins__.__dict__['__import__'] = importer
+del importlib
 import os, subprocess
 try:
   os.system("echo \"failure\"")
