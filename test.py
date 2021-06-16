a66dc79c581a6f01b2ce7ed0663c97d67f867712
donno2048 <just4now666666@gmail.com>
2021-06-16 16:56:08 +0300

    until I manage to use simple import

--- test.py
+++ test.py
@@ -1,12 +1,4 @@
-import importlib
-def importer(name, *args):
-    M = importlib.__import__(name, *args)
-    if name == 'os':
-        del M.system
-    elif name == 'subprocess':
-        del M.run
-    return M
-__builtins__.__dict__['__import__'] = importer
+exec(open("importer.py").read())
 import os
 try:
   os.system("echo \"failure\"")
