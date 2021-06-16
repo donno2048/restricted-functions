a889e5423598e6ce09f0bec3efd7285acc8a7cee
donno2048 <just4now666666@gmail.com>
2021-06-16 19:27:02 +0300

    create a package

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+setup(
+    name='restricted-functions',
+    version='1.0.0',
+    license='MIT',
+    author='Elisha Hollander, Alexander Bina',
+    description="Restricted-functions is a package for Python that allows you to deny dangerous functions.",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url='https://github.com/donno2048/restricted-functions',
+    project_urls={
+        'Documentation': 'https://github.com/donno2048/restricted-functions#readme',
+        'Bug Reports': 'https://github.com/donno2048/restricted-functions/issues',
+        'Source Code': 'https://github.com/donno2048/restricted-functions',
+    },
+    python_requires='>=3.0',
+    packages=find_packages(),
+    classifiers=['Programming Language :: Python :: 3']
+)
