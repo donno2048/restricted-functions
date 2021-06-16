fb514277c481d99f6eaa30261313c142a56af2e4
Elisha Hollander <just4now666666@gmail.com>
2021-06-16 20:30:58 +0300

    Create test.py

---
+++ test.py
@@ -0,0 +1,20 @@
+import ref
+import os, subprocess, shutil
+try: os.system("echo \"failure\"")
+except: pass
+else: raise Exception("failed")
+try: os.rmdir("../restricted-functions")
+except: pass
+else: raise Exception("failed")
+try: subprocess.run("echo \"failure\"")
+except: pass
+else: raise Exception("failed")
+try: subprocess.check_output("echo \"failure\"")
+except: pass
+else: raise Exception("failed")
+try: subprocess.call("echo \"failure\"")
+except: pass
+else: raise Exception("failed")
+try: shutil.rmtree("../restricted-functions")
+except: pass
+else: raise Exception("failed")
