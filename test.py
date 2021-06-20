b965eb58aa9a6fd2d1baafb300a72e6cc619ccee
Elisha Hollander <just4now666666@gmail.com>
2021-06-20 23:43:28 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,25 +1,12 @@
 #test it out
 import ref
-ref.main(__builtins__, protectfiles = True, protectdirs = True, lockperms = True)
+ref.main(__builtins__, protectfiles = True, protectdirs = True, lockperms = True, silent = True)
 import os, subprocess, shutil
-try: os.system("echo \"failure\"")
-except Exception as e: print(e) # Testing
-else: raise Exception("failed")
-try: os.rmdir("../restricted-functions")
-except Exception as e: print(e) # Testing
-else: raise Exception("failed")
-try: subprocess.run("echo \"failure\"")
-except Exception as e: print(e) # Testing
-else: raise Exception("failed")
-try: subprocess.check_output("echo \"failure\"")
-except Exception as e: print(e) # Testing
-else: raise Exception("failed")
-try: subprocess.call("echo \"failure\"")
-except Exception as e: print(e) # Testing
-else: raise Exception("failed")
-try: shutil.rmtree("../restricted-functions")
-except Exception as e: print(e) # Testing
-else: raise Exception("failed")
+os.system("echo \"failure\"")
+os.rmdir("../restricted-functions")
+subprocess.run("echo \"failure\"")
+subprocess.check_output("echo \"failure\"")
+subprocess.call("echo \"failure\"")
 try: open("setup.py", "w").write("text")
 except Exception as e: print(e) # Testing
 else: print("Failed to block write")
