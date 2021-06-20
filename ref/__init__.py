e4ae12b4c80696f1ffe9ff149138d4c655f72ad0
Elisha Hollander <just4now666666@gmail.com>
2021-06-20 15:29:19 +0300

    Update __init__.py

--- ref/__init__.py
+++ ref/__init__.py
@@ -1,8 +1,8 @@
 """To use this module just use the main function at the top of your code"""
 from types import ModuleType
 import importlib
-__level, __protectfiles, __protectdirs = None, None, None
-def main(__builtins__: ModuleType, protectfiles: bool = False, protectdirs: bool = False, level: int = 0) -> None:
+__protectfiles, __protectdirs = None, None
+def main(__builtins__: ModuleType, protectfiles: bool = False, protectdirs: bool = False) -> None:
     """
     # Usage
 
@@ -38,8 +38,8 @@ def main(__builtins__: ModuleType, protectfiles: bool = False, protectdirs: bool
     ```
     
     """
-    global __level, __protectfiles, __protectdirs
-    __level, __protectfiles, __protectdirs = level, protectfiles, protectdirs
+    global __protectfiles, __protectdirs
+    __protectfiles, __protectdirs = protectfiles, protectdirs
     __builtins__.__dict__['__import__'] = __import
     __builtins__.__dict__['open'] = __open
 def __open(filename, mode="r", *args, **kwargs):
@@ -47,52 +47,51 @@ def __open(filename, mode="r", *args, **kwargs):
     if __protectfiles and ("w" in mode or "a" in mode): raise AttributeError()
     return open(filename, mode, *args, **kwargs)
 def __import(name, *args):
-    global __level, __protectfiles, __protectdirs
-    level, protectfiles, protectdirs = __level, __protectfiles, __protectdirs
+    global __protectfiles, __protectdirs
+    protectfiles, protectdirs = __protectfiles, __protectdirs
     try: M = importlib.__import__(name, *args)
     except AttributeError: return __import__
-    if level >= 0:
-        if name == 'os':
-            #if it is the os module being imported
-            try: del M.system
-            except AttributeError: pass
-            try: del M.popen
-            except AttributeError: pass
-            try: del M.kill
-            except AttributeError: pass
-            try: del M.spawn
-            except AttributeError: pass
-            try: del M.execl
-            except AttributeError: pass
-            try: del M.execle
-            except AttributeError: pass
-            try: del M.execlp
-            except AttributeError: pass
-            try: del M.execlpe
-            except AttributeError: pass
-            try: del M.execv
-            except AttributeError: pass
-            try: del M.execve
-            except AttributeError: pass
-            try: del M.execvp
-            except AttributeError: pass
-            try: del M.execvpe
-            except AttributeError: pass
-            try: del M.killpg
-            except AttributeError: pass
-            try: del M.fork
-            except AttributeError: pass
-            try: del M.forkpty
-            except AttributeError: pass
-            try: del M.plock
-            except AttributeError: pass
-        elif name == 'subprocess':
-            try: del M.run
-            except AttributeError: pass
-            try: del M.check_output
-            except AttributeError: pass
-            try: del M.call
-            except AttributeError: pass
+    if name == 'os':
+        #if it is the os module being imported
+        try: del M.system
+        except AttributeError: pass
+        try: del M.popen
+        except AttributeError: pass
+        try: del M.kill
+        except AttributeError: pass
+        try: del M.spawn
+        except AttributeError: pass
+        try: del M.execl
+        except AttributeError: pass
+        try: del M.execle
+        except AttributeError: pass
+        try: del M.execlp
+        except AttributeError: pass
+        try: del M.execlpe
+        except AttributeError: pass
+        try: del M.execv
+        except AttributeError: pass
+        try: del M.execve
+        except AttributeError: pass
+        try: del M.execvp
+        except AttributeError: pass
+        try: del M.execvpe
+        except AttributeError: pass
+        try: del M.killpg
+        except AttributeError: pass
+        try: del M.fork
+        except AttributeError: pass
+        try: del M.forkpty
+        except AttributeError: pass
+        try: del M.plock
+        except AttributeError: pass
+    elif name == 'subprocess':
+        try: del M.run
+        except AttributeError: pass
+        try: del M.check_output
+        except AttributeError: pass
+        try: del M.call
+        except AttributeError: pass
     if protectfiles:
         #if file deletion/modification is blocked, block these functions
         if name == 'os':
