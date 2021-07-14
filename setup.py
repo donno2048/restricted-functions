9d87bf5ab1f5c748ac3d878d657474d7d20a3d12
Elisha Hollander <just4now666666@gmail.com>
2021-07-14 14:34:55 +0300

    allow uninstalling

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 from setuptools.command.install import install
 from ref import __version__
 class Install(install):
-    DATA = "__import__('sys').modules['__main__'].__builtins__.__dict__['ref'] = __import__('ref')"
+    DATA = "\ntry: __import__('sys').modules['__main__'].__builtins__.__dict__['ref'] = __import__('ref')\nexcept ModuleNotFoundError: pass"
     def run(self):
         install.run(self)
         if self.DATA not in open(__import__('site').__file__, 'r').read().splitlines(): open(__import__('site').__file__, 'a').write("\n" + self.DATA)
