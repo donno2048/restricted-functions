6dabc2addfc81c53fa0a2dedd93a80969da889ba
Elisha Hollander <just4now666666@gmail.com>
2021-08-19 01:02:03 +0300

    Update main.py

--- ref/main.py
+++ ref/main.py
@@ -1,13 +1,14 @@
 try: import __init__
 except ModuleNotFoundError: pass
-__ref__(*range(4))
-from sys import version, platform
-from code import InteractiveConsole
-def main():
-    print("Python %s on %s\nType \"help\", \"copyright\", \"credits\" or \"license\" for more information." % (version, platform))
-    inter, flag = InteractiveConsole(), False
-    while True:
-        try: flag = inter.push(input(">>> " if not flag else "... "))
-        except KeyboardInterrupt: print("\nKeyboardInterrupt")
-        except EOFError: break
-if __name__ == '__main__': main()
+else:
+    __ref__(*range(4))
+    from sys import version, platform
+    from code import InteractiveConsole
+    def main():
+        print("Python %s on %s\nType \"help\", \"copyright\", \"credits\" or \"license\" for more information." % (version, platform))
+        inter, flag = InteractiveConsole(), False
+        while True:
+            try: flag = inter.push(input(">>> " if not flag else "... "))
+            except KeyboardInterrupt: print("\nKeyboardInterrupt")
+            except EOFError: break
+    if __name__ == '__main__': main()
